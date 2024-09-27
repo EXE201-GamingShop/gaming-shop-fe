dffcdb69cea12e60b15752b9e3c0950ce8dc4777
DuyDucLH <duyduc.luonghuu@gmail.com>
2024-09-27 20:51:20 +0700

    refactor: import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Footer, Header, JsPreloader } from "./components";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from "./pages/Home";
-import Login from "./pages/Login";  
+import { Home, Login } from "./pages";
 
 function App() {
   return (
